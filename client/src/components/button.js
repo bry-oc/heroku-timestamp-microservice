d494f9049eedece397c240e232db7e44e8d72fe9
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-22 12:41:40 -0400

    Connected api to frontend. Added test api call. Changed react title

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,7 +1,17 @@
+import React from "react";
+
 function Button(){
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
+    
     return(
         <div>
-            <p>Timestamp Display</p>
+            <p>Timestamp Display: {!data ? "" : data}</p>
             <form>
                 <label for="timestamp">Enter a timestamp. (See examples.)<br></br></label>
                 <input type="text" id="timestamp" name="timestamp" value="timestamp examples"></input>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+app.get("/api", function(req, res){
+    res.json({message: "Hello World"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
