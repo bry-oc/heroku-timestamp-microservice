f739adae0eacc2177981c16cb8566459b1bc6eb4
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-24 17:57:15 -0400

    Added example and rearranged display

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -2,7 +2,7 @@ function Welcome(){
     return(
         <div className="container welcome">
             <hr></hr>
-            <p><b>Welcome! </b>This is a site setup to display the UTC time and Unix timestamp of a given date.  You may enter a date below and have it display on this webpage and you can also send a request to our server to see the response (<a href="https://timestamp-microservice-bryoc.herokuapp.com/api/">https://timestamp-microservice-bryoc.herokuapp.com/api/</a>&lt;date&gt;, where &lt;date&gt; is replaced by your given date). You may also enter nothing and you will recieve the UTC time and Unix timestamp of the current time.</p>
+            <p><b>Welcome! </b>This is a site setup to display the UTC time and Unix timestamp of a given date.  You may enter a date below and have it displayed on this webpage.  You may also enter nothing and you will recieve the UTC time and Unix timestamp of the current time. You can also send a request to our server to see the response (<a href="https://timestamp-microservice-bryoc.herokuapp.com/api/" target="_blank">https://timestamp-microservice-bryoc.herokuapp.com/api/</a>&lt;date&gt;, where &lt;date&gt; is replaced by your given date).</p>
             <hr></hr>
         </div>
     );
