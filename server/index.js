d1c98d070679f70bbb46840642fd02b8388798fb
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-24 17:07:28 -0400

    changed scripts

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
