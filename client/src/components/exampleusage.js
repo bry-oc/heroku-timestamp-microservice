7489aa272f39dd4f2d39e4e3173a1bc045c5fa86
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-24 18:43:25 -0400

    Made example format more clear

--- client/src/components/exampleusage.js
+++ client/src/components/exampleusage.js
@@ -9,7 +9,7 @@ function ExampleUsage(){
             <p>UTC is the primary time standard used across the world.  It is within one second of mean solar time at 0&#176; longitude and does not adjust for daylight saving time.  Greenwich Mean Time is the time zone where the UTC standard is measured.</p>
             <ul>
                 <li>Enter a date following YYYY-MM-DD format (e.g. 2021-01-02 or <a href="https://timestamp-microservice-bryoc.herokuapp.com/api/2021-01-02" target="_blank">https://timestamp-microservice-bryoc.herokuapp.com/api/2021-01-02</a></li>
-                <li>Any input containing, a year, a month(numbered or named), and a day in conventional language</li>
+                <li>A valid date consisting of a year, a month(numbered or named), and a day in conventional language</li>
             </ul>
             <h3>Unix Timestamps</h3>
             <p>Unix time is the number of seconds that have elapsed since the Unix epoch, minus leap seconds.  The Unix epoch is 00:00:00 UTC on 1 January 1970.</p>
